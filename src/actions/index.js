9e82d2237dd574424f3fbaca54427b97fbd94d66
Charles Thomas <charles@charlesmthomas.com>
2017-06-15 12:42:39 -0400

    Install Axios library and set fetchWeather payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '2d2b2f6dbf2fc546b187848c26cc4d27';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+      type: FETCH_WEATHER,
+      payload: request
+    };
+}
