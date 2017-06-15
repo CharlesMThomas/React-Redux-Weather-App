76effa8dfe251400e6f54582411837783bf2205f
Charles Thomas <charles@charlesmthomas.com>
2017-06-15 13:15:35 -0400

    Connect SearchBar to fetchWeather action creator
    The fetchWeather action was binded to the SearchBar container in
    order to initial get request via the axios library from the weather
    API.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '2d2b2f6dbf2fc546b187848c26cc4d27';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
@@ -10,12 +13,13 @@ export default class SearchBar extends Component {
     onFormSubmit(event) {
         event.preventDefault();
 
-        // Todo: Need to make an API request to retrieve weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
     }
 
     render() {
         return (
-            <form onSubmit={this.onFormSubmit} className="input-group">
+            <form onSubmit={event => this.onFormSubmit(event)} className="input-group">
                 <input 
                     placeholder = 'Get a five-day forecast in your favorite cities'
                     className="form-control"
@@ -29,3 +33,10 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDisptachToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// mapDispatchToProps must be the second agrument to connet
+export default connect(null, mapDisptachToProps)(SearchBar);
